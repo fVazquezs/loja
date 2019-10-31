c9265e536fa9995df99e852099720062876929d4
Fernando Vázquez <vzs.fernando@gmail.com>
2019-10-31 11:08:42 -0300

    adding project

---
+++ ClientApp/src/components/Layout.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Col, Grid, Row } from 'react-bootstrap';
+import { NavMenu } from './NavMenu';
+
+export class Layout extends Component {
+  displayName = Layout.name
+  componentDidMount() {
+    console.log(this.props)
+  }
+  render() {
+    return (
+      <Grid fluid>
+        <Row>
+          <Col sm={3}>
+            <NavMenu />
+          </Col>
+          <Col sm={9}>
+            {this.props.children}
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
---
+++ Data/lojaContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace loja.Models
+{
+    public class lojaContext : DbContext
+    {
+        public lojaContext (DbContextOptions<lojaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<loja.Models.User> User { get; set; }
+    }
+}
