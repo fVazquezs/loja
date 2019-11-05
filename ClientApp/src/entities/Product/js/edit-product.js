c355b563f67be00420f88e1712057087f8b906f1
Fernando Vázquez <vzs.fernando@gmail.com>
2019-11-05 09:12:48 -0300

    products api and starting UI

---
+++ ClientApp/src/entities/Product/js/edit-product.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Redirect } from 'react-router';
+import { Button } from 'react-bootstrap';
+import { EntityDataService } from '../../../service/entities-data-service';
+import '../css/edit-product.css';
+
+export class EditProduct extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.productDataSevice = new EntityDataService("Products");
+        this.state = { id: window.location.href.split('/')[window.location.href.split('/').length - 1], name: '', redirectListProducts: false }
+    }
+
+    async componentDidMount() {
+        const product = await this.productDataSevice.getById(this.state.id);
+        this.setState({ name: product.name, loading: false })
+    }
+
+    editProduct = async (e) => {
+        e.preventDefault();
+        const response = await this.productDataSevice.update({ id: this.state.id, name: this.state.name });
+        if (response.status === 204) {
+            this.setState({ redirectListProducts: true })
+        }
+    }
+
+    render() {
+        if (this.state.redirectListProducts) {
+            return <Redirect to='/products' />
+        }
+        return (
+            <div>
+                <h1>Edit Product</h1>
+                <form onSubmit={this.editProduct}>
+                    <label className="new-product-form-label" htmlFor="new-product-name">
+                        Name:
+                        <input className="new-product-form-input" id="new-product-name" type="text" name="name" value={this.state.name} onChange={(e) => this.setState({ name: e.target.value })} />
+                    </label>
+                    <div className="edit-product-button">
+                        <Button variant="primary" type="submit">Submit</Button>
+                        <Button className="cancel-edit-product" variant="primary" onClick={() => this.setState({ redirectListProducts: true })}>Cancel</Button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ Migrations/20191104221507_product.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace loja.Migrations
+{
+    public partial class product : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    Price = table.Column<double>(nullable: false),
+                    CategoryId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Product_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_CategoryId",
+                table: "Product",
+                column: "CategoryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
