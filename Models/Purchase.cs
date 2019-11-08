26588ec7761deaf5dcf87cb1f48f41ada6a1ea34
Fernando Vázquez <fernando.vazquez@adp.com>
2019-11-08 19:07:54 -0300

    Some things that may help

--- Models/Purchase.cs
+++ Models/Purchase.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace loja.Models
 {
+    [JsonObject(IsReference = true)]
     public class Purchase
     {
+        [Key]
         public int Id { get; set; }
         public int UserId { get; set; }
         [ForeignKey("UserId")]
@@ -17,6 +21,7 @@ namespace loja.Models
 
     }
 
+    [DataContract(IsReference = true)]
     public class ProductPurchase
     {
         [Key, Column(Order = 0)]
